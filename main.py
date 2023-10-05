a5404b90d37a93052135d3bd9be51a56cc28cb4a
shnepsel <petrov.tim.ser@gmail.com>
2023-10-05 12:32:36 +0300

    venv fix

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import openai
 openai.api_base = "https://api.daku.tech/v1"
 
 # Получаем API ключ OpenAI из переменной окружения
-openai.api_key = "sk-QjBGRDU3Q2JmNzY2T3BlbkFJMkNFQjY1MzY3MTNh"
+openai.api_key = "sk-MjNhYTdlZjA4RWQ3T3BlbkFJYjY3MDJEOGQ5MUQ2"
 
 # Создаем экземпляр телеграм бота
 bot = telebot.TeleBot("6597127566:AAHtsyPwdBzidgv9MCXAPUwchk8QfeAtG3c")
